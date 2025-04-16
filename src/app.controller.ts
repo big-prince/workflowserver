e01d06329fcc4c4205ce45c82bc1cde4e70df7d4
Prince <ograindy@gmail.com>
2025-04-16 10:52:40 +0100

    feat(redis): integrate Redis client with dynamic configuration and health check endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,23 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject } from '@nestjs/common';
 import { AppService } from './app.service';
+import Redis from 'ioredis';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    @Inject('REDIS_CLIENT') private readonly redis: Redis,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('redis-health')
+  async redisHealth() {
+    console.log('Redis Health Check');
+    const status = await this.redis.ping();
+    return { status };
   }
 }
